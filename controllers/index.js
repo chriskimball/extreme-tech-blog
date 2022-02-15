8273f7a72a870e67aeb404784d8bf9a9294edac0
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-02-15 01:20:01 -0800

    Minor updates to boilerplate files

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
+
 const apiRoutes = require('./api');
+const htmlRoutes = require('./html-routes.js');
 
 // PUT, DELETE, POST requests go into API routes
 router.use('/api', apiRoutes);
 
 // HTML-Routes File will be all of our GET requests
+router.use('/', htmlRoutes);
 
 module.exports = router;
