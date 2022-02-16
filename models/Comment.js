336ff1e4e139f2403fa083c4f35def20b5e07bf5
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-02-15 23:52:25 -0800

    Added comment model

--- models/Comment.js
+++ models/Comment.js
@@ -6,14 +6,41 @@ class Comment extends Model {}
 //TODO:
 Comment.init(
   {
-    
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: 'comment',
   }
 );
 
