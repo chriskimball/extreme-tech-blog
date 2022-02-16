8429cd5a1eac06fdfc6fdfe5e9e2188de1c98987
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-02-16 02:19:01 -0800

    Added where parameter for homepage lookup of all posts for specific user

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -5,9 +5,13 @@ const withAuth = require('../utils/auth');
 // GET - "/" - home
 // home.handlebars
 router.get('/', async (req, res) => {
+  if (req.session.logged_in) {
     try {
       // Get all projects and JOIN with user data
       const postData = await Post.findAll({
+        where: {
+          user_id: req.session.user_id,
+        },
         include: [
           {
             model: User,
@@ -15,38 +19,47 @@ router.get('/', async (req, res) => {
           },
         ],
       });
-  
+
       // Serialize data so the template can read it
       const posts = postData.map((post) => post.get({ plain: true }));
-  
       // Pass serialized data and session flag into template
-      res.render('home', { 
-        posts, 
-        logged_in: req.session.logged_in 
+      res.render('home', {
+        posts,
+        logged_in: req.session.logged_in,
       });
     } catch (err) {
       res.status(500).json(err);
     }
-  });
-
+  } else {
+    res.render('home');
+  }
+});
 
 //TODO:
 // GET - "/login" - login form
 // login.handlebars
 router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
-  });
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
 
 //TODO:
 // GET - "/signup" - Registration Form
 // register.handlebars
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
 
+  res.render('register');
+});
 //TODO:
 // GET - "/dashboard" - user dashboard
 // dashboard.handlebars
